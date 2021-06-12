d7c72cda8bdeac043d51fa1a5e7b9c5fea0d5502
HarisankarSNair <85512238+HarisankarSNair@users.noreply.github.com>
2021-06-12 20:45:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,22 +10,22 @@ if uploaded_file is not None :
     #input = cv2.imwrite('input.jpg',image)
     st.image(img,caption="Uploaded Image",use_column_width=None)
 
-#if st.button("Sketch Image"):
-    # img1 = cv2.imread(input)  # reading the image file
+if st.button("Sketch Image"):
+     img1 = cv2.imread(input)  # reading the image file
 
-   # gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # converting the image to gray scale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  # converting the image to gray scale
 
-    #inverted_gray_image = 255 - gray_image  # converting the gray scale to inverted gray scale/negative value of gray scale
+    inverted_gray_image = 255 - gray_image  # converting the gray scale to inverted gray scale/negative value of gray scale
 
-    #blurred_img = cv2.GaussianBlur(inverted_gray_image, (21, 21), 0)  # Converting the inverted gray scale to blurred image
+    blurred_img = cv2.GaussianBlur(inverted_gray_image, (21, 21), 0)  # Converting the inverted gray scale to blurred image
 
-   # inverted_blurred_img = 255 - blurred_img  # converting the blurred image to its inverted image
+    inverted_blurred_img = 255 - blurred_img  # converting the blurred image to its inverted image
 
-   # pencil_sketch = cv2.divide(gray_image, inverted_blurred_img,scale=256.0)  # Dividing the gray scale by the inverted blurred image to create the pencil sketch
+    pencil_sketch = cv2.divide(gray_image, inverted_blurred_img,scale=256.0)  # Dividing the gray scale by the inverted blurred image to create the pencil sketch
 
-   # output = Image.open(pencil_sketch)
+    output = Image.open(pencil_sketch)
 
-  #  st.image(output,"Sketch Image",use_column_width=None)
+    st.image(output,"Sketch Image",use_column_width=None)
 
 
 
